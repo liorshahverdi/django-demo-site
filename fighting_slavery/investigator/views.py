7747ee5bd5d2b1796cb69721aa354681a84eb12d
Lior Shahverdi <lior@Liors-MacBook-Pro.local>
2017-06-15 22:40:28 -0400

    index view now serves html file, test for such scenario passes

--- fighting_slavery/investigator/views.py
+++ fighting_slavery/investigator/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import *
+
 # Create your views here.
 def index(request):
-	return HttpResponse("Hey you're at the investigator index.")
+	return render(request, 'investigator/index.html', {})
