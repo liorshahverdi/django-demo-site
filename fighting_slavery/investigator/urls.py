af284ace008cbce02077861a38c6c3de3a4091bb
Lior Shahverdi <liorshahverdi@gmail.com>
2017-06-15 23:21:38 -0400

    added static files, changed html for index view

--- fighting_slavery/investigator/urls.py
+++ fighting_slavery/investigator/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'investigator'
 urlpatterns = [
-	url(r'^$', views.index, name='index')
+	url(r'^$', views.index, name='index'),
+	url(r'^search/$', views.search, name='search'),
 ]
--- fighting_slavery/investigator/views.py
+++ fighting_slavery/investigator/views.py
@@ -6,3 +6,6 @@ from .models import *
 # Create your views here.
 def index(request):
 	return render(request, 'investigator/index.html', {})
+
+def search(request):
+	return HttpResponse("You have made it to the search index.")
