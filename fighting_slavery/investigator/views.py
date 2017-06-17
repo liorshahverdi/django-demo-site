a828cdfba6ba009ed5ee97d84767dd2b4335a339
Eric Schles <ericschles@gmail.com>
2017-06-17 16:35:27 -0400

    updating views to use spaces not tabs

--- fighting_slavery/investigator/views.py
+++ fighting_slavery/investigator/views.py
@@ -5,7 +5,7 @@ from .models import *
 
 # Create your views here.
 def index(request):
-	return render(request, 'investigator/index.html', {})
+    return render(request, 'investigator/index.html', {})
 
 def search(request):
-	return HttpResponse("You have made it to the search index.")
+    return HttpResponse("You have made it to the search index.")
